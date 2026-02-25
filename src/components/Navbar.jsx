fc6732d319a10970dd2df22eddddce18b6f0dbac
Snehal-londhe111 <snehallondhe111@gmail.com>
2026-02-25 16:23:21 +0530

    create home,about,skills,projects,contact

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,45 @@
+import { nav } from 'framer-motion/client'
 import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className='fixed w-full z-50 bg-black/80 blackdrop-blur-sm py-4 px-8 shadow-lg'>
+        <div className='container mx-auto flex justify-between item-center'>
+            <div>
+                <a href="#" className='text-3xl font-bold text-white'>
+                    Snehal
+                    <span className='text-purple-500'>Londhe</span>
+                    <div className='w-4 h-4 bg-purple-600 rounded-full'></div>
+                </a>
+            </div>
+            <div className='hidden md:flex space-x-10'>
+                <a href='#home' className='relative text-white/100 transition duration-300 hover:text-purple-500 group'>    
+                    <span>Home</span>
+                    <span className="absolute left-0 bottom-5 w-0 h-0.5 bg-purple-500 transition-all duration-300 group-hover:w-full"></span>
+                </a>
+
+                <a href='#about' className='relative text-white/100 transition duration-300 hover:text-purple-500 group'>    
+                    <span>About</span>
+                    <span className="absolute left-0 bottom-5 w-0 h-0.5 bg-purple-500 transition-all duration-300 group-hover:w-full"></span>
+                </a>
+
+                <a href='#Skills' className='relative text-white/100 transition duration-300 hover:text-purple-500 group'>    
+                    <span>Skills</span>
+                    <span className="absolute left-0 bottom-5 w-0 h-0.5 bg-purple-500 transition-all duration-300 group-hover:w-full"></span>
+                </a>
+
+                <a href='#project' className='relative text-white/100 transition duration-300 hover:text-purple-500 group'>    
+                    <span>Projects</span>
+                    <span className="absolute left-0 bottom-5 w-0 h-0.5 bg-purple-500 transition-all duration-300 group-hover:w-full"></span>
+                </a>
+
+                <a href='#contact' className='relative text-white/100 transition duration-300 hover:text-purple-500 group'>    
+                    <span>Contact</span>
+                    <span className="absolute left-0 bottom-5 w-0 h-0.5 bg-purple-500 transition-all duration-300 group-hover:w-full"></span>
+                </a>
+            </div>
+        </div>
+    </nav>
   )
 }
 
