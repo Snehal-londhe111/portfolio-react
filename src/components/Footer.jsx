c76566afbf8c7bd1520e120bc075aaa130f7f169
Snehal-londhe111 <snehallondhe111@gmail.com>
2026-03-18 11:10:11 +0530

    Footer section complete

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='py-8 bg-gray-800'>
+        <div className='container mx-auto px-6 text-center'>
+            <p className='text-gray-400'>
+                &copy; 2026 Created By TheSnehalCodes. All rights reserved.
+            </p>
+        </div>
+    </div>
+  )
+}
+
+export default Footer
